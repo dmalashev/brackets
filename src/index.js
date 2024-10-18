ed5dd5da7d8de914b90771f41a549a52dbda8a4b
Dmitry Malashev <dmalashev@gmail.com>
2024-10-18 17:59:30 +0300

    feat: solve the brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let closeQueue = '';
+  let isCorrectBracket = false;
+
+  for (let char of str) {
+    isCorrectBracket = false;
+    if (char === closeQueue[0]) {
+      closeQueue = closeQueue.slice(1);
+      isCorrectBracket = true;
+    } else {
+      for (let bracketsPair of bracketsConfig) {
+        if (char === bracketsPair[0]) {
+          closeQueue = bracketsPair[1] + closeQueue;
+          isCorrectBracket = true
+        } 
+      }
+    }
+
+    if (!isCorrectBracket) {
+      return false;
+    }
+  }
+
+  return closeQueue === '';
 }
